a90fc80341388d9cc534bb0bc165534515e3ee54
Kevin <newtonkevin2010@gmail.com>
2016-10-17 20:35:57 +0000

    Added nav walker function w/ hotfix for iphone 6

--- wp-content/themes/healthystart/functions.php
+++ wp-content/themes/healthystart/functions.php
@@ -1,4 +1,14 @@
 <?php
+//Register Nav Walker
+require_once('wp_bootstrap_navwalker.php');
+function wpb_theme_setup(){
+	register_nav_menus(array(
+		'primary' => __('Primary Menu')
+		));
+	
+}
+
+add_action('after_setup_theme', 'wpb_theme_setup');
 function startwordpress_scripts() {
 	
 	wp_enqueue_style( 'index', get_template_directory_uri() . '/css/index.css' );
--- wp-content/themes/healthystart/header.php
+++ wp-content/themes/healthystart/header.php
@@ -14,7 +14,7 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="sha384-2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="sha384-VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
+    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="sha384-VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>-->
 
     <!-- Custom styles for this template -->
     <link href="navbar.css" rel="stylesheet">
@@ -53,76 +53,25 @@
     </div>
   </div>
    <nav class="navbar navbar-light bg-faded ">
-        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header">
+        <button class="navbar-toggler hidden-sm-up collapsed" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false">
           &#9776; Healthy Start
         </button>
-        <div class="collapse navbar-toggleable-xs" id="navbar-header">
+        <div class="collapse navbar-toggleable-xs" id="navbar-header" style="height: 0px;">
+           <?php
+            wp_nav_menu( array(
+                'menu'              => 'primary',
+                'theme_location'    => 'primary',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+        'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
+            );
+        ?>
           
-          <ul class="nav nav-pills m-x-auto">
-            <li class="nav-item hidden-md-down">
-              <a class="nav-link  " ></a>
-            </li>
-            <li class="nav-item hidden-md-down">
-              <a class="nav-link  " ></a>
-            </li>
-            <li class="nav-item hidden-md-down">
-              <a class="nav-link  " ></a>
-            </li>
-            <li class="nav-item hidden-md-down">
-              <a class="nav-link  "></a>
-            </li>
-            <li class="nav-item hidden-md-down">
-              <a class="nav-link  " ></a>
-            </li>
-           <li class="nav-item hidden-md-down">
-            <a class="nav-link  "></a>
-          </li>
-         <li class="nav-item hidden-md-down">
-            <a class="nav-link  " ></a>
-        </li>
 
-  <li class="nav-item">
-    <a class="nav-link active" href="index.html">Home</a>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="about/why-hst.html">Why Health Start</a>
-      <a class="dropdown-item" href="about/hst-team.html">Healthy Start Team</a>
-      <a class="dropdown-item" href="about/FAQ.html">Frequently asked Questions</a>
-      <a class="dropdown-item" href="about/testimonials.html">Testimonials</a>
-      <a class="dropdown-item" href="about/relationships.html">Relationships</a>
-      <a class="dropdown-item" href="about/policies.html">Policies</a>
-    </div>
-  </li>
-   <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="services/transportation.html">Transportation</a>
-      <a class="dropdown-item" href="services/medical-companion.html">Medical Companion</a>
-      <a class="dropdown-item" href="services/grocery-delivery.html">Grocery Delivery</a>
-      <a class="dropdown-item" href="services/airport-companion.html">Airport Companion</a>
-      <a class="dropdown-item" href="services/personal-errands.html">Personal Errands</a>
-
-    </div>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">News & Events</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="services/transportation.html">Press Releases</a>
-      <a class="dropdown-item" href="services/medical-companion.html">Events</a>
-      <a class="dropdown-item" href="services/grocery-delivery.html">News Letter</a>
-
-    </div>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="index.html">Careers</a>
-  </li>
-  <li class="nav-item">
-    <span class="nav-link"><?php wp_list_pages( '&title_li='); ?></span>
-  </li>
-  
-</ul>
   </div>
       </nav> <!-- /navbar -->
             <div class="jumbotron">
